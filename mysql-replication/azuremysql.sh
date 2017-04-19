9d6582acb9be25162c6123223187081083b88d06
jijouohmac <jijovarghese92@gmail.com>
2017-04-19 16:57:14 +0530

    ..

--- mysql-replication/azuremysql.sh
+++ mysql-replication/azuremysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script is only tested on CentOS 6.5 and Ubuntu 12.04 LTS with Percona XtraDB Cluster 5.6.
-# You can customize variables such as MOUNTPOINT, RAIDCHUNKSIZE and so on to your needs.
+# You can customize variables such as MOUNTPOINT, RAIDCHUNKSIZE and so on to your needs..
 # You can also customize it to work with other Linux flavours and versions.
 # If you customize it, copy it to either Azure blob storage or Github so that Azure
 # custom script Linux VM extension can access it, and specify its location in the 
